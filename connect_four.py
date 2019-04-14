f9a756e476dca51280b444dbec1f716c2e652e8d
안병욱 <brians_mac_mini@anbyeong-ug-ui-Macmini.local>
2019-04-15 00:51:55 +0900

    order added

--- connect_four.py
+++ connect_four.py
@@ -1,1 +1,16 @@
-print("Hello World?")
+print("------------------------------")
+print("Artificial Intellgence Project")
+print("-------- Connect Four --------")
+print("-------- Human vs CPU --------")
+print("------------------------------")
+order = ''
+while order != 'F' and order != 'f' and order != 'S' and order != 's':
+    order = input("Will you go First or Second? (F/S) : ")
+    if order == 'F' or order == 'f':
+        print("You decided to fo First.")
+        break
+    elif order == 'S' or order == 's':
+        print("You decided to fo Second.")
+        break
+    else:
+        print("Wrong Input. Please try it again.")
