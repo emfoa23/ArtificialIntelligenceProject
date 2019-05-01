7c8440a04fa8613c7ddfb05009631f180cd08599
Brian Ahn <emfoa23@gmail.com>
2019-05-02 03:36:35 +0900

    이거저거 추가

--- connect_four.py
+++ connect_four.py
@@ -30,11 +30,15 @@ def betting(turn, map):
                             print("Column "+human_bet+" is already full. Please select another column.")
                             return map, False, last_betting_point
     else:   # 컴퓨터 차례
-
-        # 테스트용 코드
-        cpu_bet = randint(0, 6) # 1~7열 사이에 랜덤하게 착수하기
-        # cpu_bet = 6   # 6열에만 수 두기
-
+        cpu_input = -1
+        while cpu_input != '0' and cpu_input != '1':
+            cpu_input = input("Choose betting method CPU should select. (0 - random / 1 - heuristic): ")
+            if int(cpu_input) == 0:
+                cpu_bet = randint(0, 6) # 1~7열 사이에 랜덤하게 착수하기
+            elif int(cpu_input) == 1:
+                cpu_bet = randint(0, 6) # 1~7열 사이에 랜덤하게 착수하기
+            else:
+                print("Wrong Input. Please try it again.")
         for i in range(6):
             if map[i][cpu_bet] == 0:
                 map[i][cpu_bet] = -1
@@ -143,12 +147,49 @@ def gameOver(map, last_betting_point, turn):
     else:
         return False, winner
 
+# state 설정 함수
+def initialMapSetter(map, state, turn):
+    dot = [['.'] * 7 for i in range(6)]
+    for i in range(len(state)):
+        if turn == 1:
+            for j in range(6):
+                if map[j][int(state[i]) - 1] == 0:
+                    map[j][int(state[i]) - 1] = 1
+                    break
+        else:
+            for j in range(6):
+                if map[j][int(state[i]) - 1] == 0:
+                    map[j][int(state[i]) - 1] = -1
+                    break
+        turn *= -1
+    for i in range(6):
+        for j in range(7):
+            if map[i][j] == 1:
+                dot[i][j] = '○'
+            elif map[i][j] == -1:
+                dot[i][j] = '●'
+            else:
+                dot[i][j] = '.'
+    print()
+    print("# 1 2 3 4 5 6 7 #")
+    print('F', dot[5][0], dot[5][1], dot[5][2], dot[5][3], dot[5][4], dot[5][5], dot[5][6], '#')
+    print('E', dot[4][0], dot[4][1], dot[4][2], dot[4][3], dot[4][4], dot[4][5], dot[4][6], '#')
+    print('D', dot[3][0], dot[3][1], dot[3][2], dot[3][3], dot[3][4], dot[3][5], dot[3][6], '#')
+    print('C', dot[2][0], dot[2][1], dot[2][2], dot[2][3], dot[2][4], dot[2][5], dot[2][6], '#')
+    print('B', dot[1][0], dot[1][1], dot[1][2], dot[1][3], dot[1][4], dot[1][5], dot[1][6], '#')
+    print('A', dot[0][0], dot[0][1], dot[0][2], dot[0][3], dot[0][4], dot[0][5], dot[0][6], '#')
+    print("# # # # # # # # #")
+    print()
+    print("You: ○ , CPU: ●")
+    return map, turn
+
 # 게임 실행 함수
 def startGame():
     text_input = ''
     turn = 0
     map = [[0] * 7 for i in range(6)]
     state = []
+    set_intial_state = ''
     while text_input != 'F' and text_input != 'f' and text_input != 'S' and text_input != 's':
         text_input = input("Will you go First or Second? (F/S) : ")
 
@@ -173,6 +214,15 @@ def startGame():
             print("CPU go First.")
             turn = -1
             break
+        else:
+            print("Wrong Input. Please try it again.")
+    while set_intial_state != 'Y' and set_intial_state != 'y' and set_intial_state != 'N' and set_intial_state != 'n':
+        set_intial_state = input("Do you want to set initial state? (Y/N): ")
+        if set_intial_state == 'Y' or set_intial_state == 'y':
+            state = [int(x) for x in input("Initial State (ex. 4435): ")]
+            map, turn = initialMapSetter(map, state, turn)
+        elif set_intial_state == 'N' or set_intial_state == 'n':
+            state = []
         else:
             print("Wrong Input. Please try it again.")
     while True: # 게임 진행 함수
