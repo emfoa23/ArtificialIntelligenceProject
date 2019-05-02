79b9905ecd1ddce3407afc8e1ef6134dce6eda4e
Brian Ahn <emfoa23@gmail.com>
2019-05-02 16:25:46 +0900

    주석 추가

--- connect_four.py
+++ connect_four.py
@@ -1,7 +1,7 @@
 # Import
 from random import randint
 
-# 착수 가능한 열을 찾아주는 함수
+# 착수 가능한 곳을 찾아주는 함수
 def declareAvailableBettingPoint(map, available_betting_point_address, option):
     new_available_betting_point_address = []
     # initial state이 주어지지 않았을때
@@ -45,7 +45,6 @@ def betting(turn, map, available_betting_point_address):
                         return map, True, last_betting_point
                 print("Column " + human_bet + " is already full. Please select another column.")
                 return map, False, last_betting_point
-
     # 컴퓨터 차례
     else:
         cpu_input = -1
@@ -198,17 +197,21 @@ def initialMapSetter(map, state, turn):
 # 게임 실행 함수
 def startGame():
     text_input = ''
-    turn = 0
-    map = [[0] * 7 for i in range(6)]
-    state = []
-    set_intial_state = ''
-    available_betting_point_address = [[0,1],[0,2],[0,3],[0,4],[0,5],[0,6],[0,7]]
+    turn = 0    # 누구 차례인지 알려주는 변수. CPU: -1, 사람: 1
+    map = [[0] * 7 for i in range(6)]   # 게임판. 빈칸: 0, CPU가 둔곳: -1, 사람이 둔곳: 1
+    state = []  # 게임판의 착수됐던 열들을 알려주는 리스트
+
+    # 테스트용 코드
+    set_intial_state = ''   # 시작 state를 설정해주는 변수
+
+    available_betting_point_address = [[0,1],[0,2],[0,3],[0,4],[0,5],[0,6],[0,7]]   # 착수가능한 위치를 알려주는 리스트
     while text_input != 'F' and text_input != 'f' and text_input != 'S' and text_input != 's':
         text_input = input("Will you go First or Second? (F/S) : ")
 
         # 테스트용 코드
         # text_input = 'f'    # 무조건 선공하기
 
+        # 사람이 먼저 시작할때
         if text_input == 'F' or text_input == 'f':
             print()
             print("# 1 2 3 4 5 6 7 #")
@@ -223,39 +226,50 @@ def startGame():
             print("You go First.")
             turn = 1
             break
+        # CPU가 먼저 시작할때
         elif text_input == 'S' or text_input == 's':
             print("CPU go First.")
             turn = -1
             break
         else:
             print("Wrong Input. Please try it again.")
+
+    # 테스트용 코드
     while set_intial_state != 'Y' and set_intial_state != 'y' and set_intial_state != 'N' and set_intial_state != 'n':
         set_intial_state = input("Do you want to set initial state? (Y/N): ")
+        # 초기 state를 설정할때
         if set_intial_state == 'Y' or set_intial_state == 'y':
             state = [int(x) for x in input("Initial State (ex. 4435): ")]
             map, turn = initialMapSetter(map, state, turn)
             option = 1
+        # 초기 state를 설정하지 않을때
         elif set_intial_state == 'N' or set_intial_state == 'n':
             state = []
             option = 0
         else:
             print("Wrong Input. Please try it again.")
-    while True: # 게임 진행 함수
+    while True:
+        # 착수 가능한 곳을 찾아주는 함수
         available_betting_point_address, option = declareAvailableBettingPoint(map, available_betting_point_address, option)
 
         # 테스트용 코드
         print("Available betting point:",available_betting_point_address)   # 배팅가능 포인트 출력
 
+        # 게임 진행
         map, turn, last_betting_point, state = gameProgressing(map, turn, state, available_betting_point_address)
+        # 게임오버 됐는지를 알려주는 함수
         game_over, winner = gameOver(map, last_betting_point, turn)
-        if game_over:   # 게임이 끝나면 누가 이겼는지 출력
+        if game_over:
             print()
             print("---------------------------------------")
             print("-------------- Game Over --------------")
+            # 사람이 이겼을 경우
             if winner == 1:
                 print("--------------- You Win ---------------")
+            # CPU가 이겼을 경우
             elif winner == -1:
                 print("--------------- CPU Win ---------------")
+            # 비겼을 경우
             else:
                 print("---------------- Draw! ----------------")
             print("---------------------------------------")
@@ -263,7 +277,7 @@ def startGame():
             break
 
 # 메인 함수
-game_continue = ''
+game_continue = ''  # 게임이 한판 끝났을때 재시작할지에 대한 변수
 print()
 print("----------------------------------------")
 print("---- Artificial Intellgence Project ----")
@@ -271,6 +285,7 @@ print("------------- Connect Four -------------")
 print("------------- Human vs CPU -------------")
 print("----------------------------------------")
 print()
+# 게임 실행 함수
 startGame()
 while game_continue != 'Y' and game_continue != 'y' and game_continue != 'N' and game_continue != 'n':
     game_continue = input("Do you want to restart the game? (Y/N) : ")
@@ -278,6 +293,7 @@ while game_continue != 'Y' and game_continue != 'y' and game_continue != 'N' and
     # 테스트용 코드
     # game_continue = 'y' # 게임 무한 재실행
 
+    # 게임 재시작을 원할때
     if game_continue == 'Y' or game_continue == 'y':
         print()
         print("----------------------------------------")
@@ -286,6 +302,7 @@ while game_continue != 'Y' and game_continue != 'y' and game_continue != 'N' and
         print()
         game_continue = ''
         startGame()
+    # 게임 재시작을 원하지 않을때
     elif game_continue == 'N' or game_continue == 'n':
         print()
         print("----------------------------------------")
